806abdbff6c453e7cfd72cac9ab4072ce473d32c
tobiaj <tobiaj@kth.se>
2016-11-09 11:34:47 +0100

    Start

---
+++ src/CuratorAgent.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tobiaj on 2016-11-09.
+ */
+public class CuratorAgent {
+}
---
+++ src/ProfilerAgents.java
@@ -0,0 +1,21 @@
+import jade.core.Agent;
+
+/**
+ * Created by tobiaj on 2016-11-09.
+ */
+public class ProfilerAgents extends Agent{
+    private String age;
+    private String occupation;
+    private String gender;
+    private String interests;
+    private String[] visitedItems;
+
+    public ProfilerAgents(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
